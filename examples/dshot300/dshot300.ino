85814f39be5cbc5f7d76ba2c6985dff7d5142eb4
Wastl Kraus <derdoktor667@gmail.com>
2022-11-25 16:08:58 +0100

    ...cosmetics

--- examples/dshot300/dshot300.ino
+++ examples/dshot300/dshot300.ino
@@ -14,15 +14,17 @@ DShotRMT dshot_01(GPIO_NUM_4, RMT_CHANNEL_0);
 volatile uint16_t throttle_value = 0x30; // ...sending "48", the first throttle value
 constexpr auto FAILSAVE_THROTTLE = 0x3E7;
 
-void setup() {
+void setup()
+{
     // ...always start the onboard usb support
-	USB_Serial.begin(USB_SERIAL_BAUD);
+    USB_Serial.begin(USB_SERIAL_BAUD);
 
     // ...start the dshot generation
     dshot_01.begin(DSHOT300);
 }
 
-void loop() {
+void loop()
+{
     read_SerialThrottle();
 
     dshot_01.send_dshot_value(throttle_value);
@@ -33,11 +35,15 @@ void loop() {
 
 //
 //
-uint16_t read_SerialThrottle() {
-	if (USB_Serial.available() > 0) {
-		auto throttle_input = (USB_Serial.readStringUntil('\n')).toInt();
-		return throttle_input;
-	} else {
+uint16_t read_SerialThrottle()
+{
+    if (USB_Serial.available() > 0)
+    {
+        auto throttle_input = (USB_Serial.readStringUntil('\n')).toInt();
+        return throttle_input;
+    }
+    else
+    {
         return FAILSAVE_THROTTLE;
     }
 }

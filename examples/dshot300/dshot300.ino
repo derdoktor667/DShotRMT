34c6f66256a49da3763f401f1b4fe4d58206ecca
Wastl Kraus <derdoktor667@gmail.com>
2023-03-26 16:19:54 +0200

    ...updated class to optimize configuration

--- examples/dshot300/dshot300.ino
+++ examples/dshot300/dshot300.ino
@@ -9,7 +9,7 @@
 constexpr auto USB_SERIAL_BAUD = 115200;
 #endif // SERIAL
 
-DShotRMT dshot_01(GPIO_NUM_4, RMT_CHANNEL_0);
+DShotRMT motor01(GPIO_NUM_4, RMT_CHANNEL_0, DSHOT300, false);
 
 volatile uint16_t throttle_value = 0x30; // ...sending "48", the first throttle value
 constexpr auto FAILSAVE_THROTTLE = 0x3E7;
@@ -18,16 +18,13 @@ void setup()
 {
 	// ...always start the onboard usb support
 	USB_Serial.begin(USB_SERIAL_BAUD);
-
-	// ...start the dshot generation
-	dshot_01.begin(DSHOT300);
 }
 
 void loop()
 {
 	read_SerialThrottle();
 
-	dshot_01.send_dshot_value(throttle_value);
+	motor01.send_dshot_value(throttle_value);
 
 	// ...print to console
 	USB_Serial.println(throttle_value);

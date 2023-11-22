fef2653c407fb22a78c38e1b82b919d356ae5763
Edward Stuckey <observer21.public@gmail.com>
2023-11-22 03:25:26 -0500

    testing environment updates
    nothing important was changed, just some code I was using to test it.

--- dev/ESPDshot-Testing/src/main.cpp
+++ dev/ESPDshot-Testing/src/main.cpp
@@ -11,6 +11,7 @@ const int USB_SERIAL_BAUD = 115200;
 
 // Define the GPIO pin connected to the motor and the DShot protocol used
 const auto MOTOR01_PIN = 23;
+const auto MOTOR02_PIN = 18;
 const auto DSHOT_MODE = DSHOT600;
 
 // Define the failsafe and initial throttle values
@@ -19,11 +20,13 @@ const auto INITIAL_THROTTLE = 48;
 
 
 DShotRMT anESC(MOTOR01_PIN);
+DShotRMT anotherESC(MOTOR02_PIN);
 
 void setup()
 {
 	USB_Serial.begin(USB_SERIAL_BAUD);
 	anESC.begin(DSHOT_MODE, ENABLE_BIDIRECTION, 14);
+	anotherESC.begin(DSHOT_MODE, ENABLE_BIDIRECTION, 14);
 
 }
 
@@ -33,19 +36,26 @@ void loop()
 	if(loopCount < 700)
 	{
 		anESC.send_dshot_value(INITIAL_THROTTLE);
+		anotherESC.send_dshot_value(INITIAL_THROTTLE);
 	}
 	else if(loopCount < 1200)
 	{
 
     	anESC.send_dshot_value(500);
+		anotherESC.send_dshot_value(300);
 	}
 	else
 	{
 		anESC.send_dshot_value(300);
+		anotherESC.send_dshot_value(500);
 	}
 
 	if(loopCount % 10 == 0)
-		Serial.println(anESC.get_dshot_RPM());
+	{
+		Serial.print(anESC.get_dshot_RPM());
+		Serial.print(" || ");
+		Serial.println(anotherESC.get_dshot_RPM());
+	}
 
 	delay(10);
 		++loopCount;
